b4602fc9ea1aaa5f0bd8c1411199280fd6c33001
brahmihub <151893249+brahmihub@users.noreply.github.com>
2023-12-13 20:41:22 +0100

    Add files via upload

---
+++ commands/core/help.js
@@ -0,0 +1,21 @@
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    name: 'help',
+    description: "All the commands this bot has!",
+    showHelp: false,
+
+    execute({ client, inter }) {
+        const commands = client.commands.filter(x => x.showHelp !== false);
+
+        const embed = new EmbedBuilder()
+        .setColor('#ff0000')
+        .setAuthor({ name: client.user.username, iconURL: client.user.displayAvatarURL({ size: 1024, dynamic: true }) })
+        
+        .addFields([ { name: `Enabled - ${commands.size}`, value: commands.map(x => `\`${x.name}\``).join(' | ') } ])
+        .setTimestamp()
+        .setFooter({ text: ' BraBot ❤️', iconURL: inter.member.avatarURL({ dynamic: true })});
+
+        inter.editReply({ embeds: [embed] });
+    },
+};
