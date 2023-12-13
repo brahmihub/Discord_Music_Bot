190c79326955680a86bcdb3fc711511c38731428
brahmihub <151893249+brahmihub@users.noreply.github.com>
2023-12-13 20:42:14 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    app: {
+        token: 'MTE3ODA0MTk4NzMwNDI2MzcyMQ.GtSBsC.rqC5ltveTezRLVkoYd2j0cgMO93OootIVbCHQQ',
+        playing: 'by Brahmi and Lara ❤️',
+        global: true,
+        guild: 'xxx',
+        ExtraMessages: false,
+        loopMessage: false,
+
+    },
+
+    opt: {
+        DJ: {
+            enabled: false,
+            roleName: '',
+            commands: []
+        },
+        maxVol: 200,
+        spotifyBridge: true,
+        volume: 75,
+        leaveOnEmpty: true,
+        leaveOnEmptyCooldown: 30000,
+        leaveOnEnd: false,
+        leaveOnEndCooldown: 30000,
+        discordPlayer: {
+            ytdlOptions: {
+                quality: 'highestaudio',
+                highWaterMark: 1 << 25
+            }
+        }
+    }
+};
